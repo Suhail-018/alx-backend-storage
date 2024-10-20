43cc90b903e5c7b6dc56e227d0065ca29596e6ec
suhail-018 <suhailysa@gmail.com>
2024-10-20 07:00:50 +0300

    alx-back-end

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,9 @@
 -- This SQL script select band_name, and lifespan column which is difference
 -- Select bands with Glam rock as the main style and compute lifespan
+-- Select bands with Glam rock as the main style and compute lifespan correctly
 SELECT
     name AS band_name,
-    IF(split IS NULL, 2022 - formed, split - formed) AS lifespan
+    IF(split IS NULL OR split = 0, 2023 - formed, split - formed) AS lifespan
 FROM
     bands
 WHERE
